cde319c53d5917568ae50544f8ddd4e0aadcf0c7
Gabriel-David <rosudavidg@gmail.com>
2020-02-28 22:55:27 +0200

    fixed connection between nodejs and mysql

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const PORT = 5000;
 const HOST = '0.0.0.0';
 
 const app = express();
+
+let pool = mysql.createPool({
+    host: "localhost",
+    port: 33070,
+    user: "root",
+    password: "supersecret",
+    database: "survket_db"
+});
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    pool.getConnection(function (err, connection) {
+        connection.query("SELECT * FROM users", function (err, rows) {
+            connection.release();
+            if (err) throw err;
+
+            console.log(rows.length);
+            res.send(JSON.stringify(rows));
+        });
+    });
 });
 
 app.listen(PORT, HOST);
