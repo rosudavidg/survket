2d0204ea15af017f987a491628c5d98db7e76e19
Gabriel-David <rosudavidg@gmail.com>
2020-03-20 00:15:40 +0200

    added POST /users

--- backend/Users/controller.js
+++ backend/Users/controller.js
@@ -1,12 +1,75 @@
 const express = require("express");
-
 const UsersService = require("./services.js");
-
+const { validateFields } = require("../utils");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const users = await UsersService.getAll();
-  res.json(users);
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await UsersService.getAll();
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  let {
+    first_name,
+    last_name,
+    role_id,
+    email,
+    date_of_birth,
+    gender
+  } = req.body;
+
+  try {
+    validateFields({
+      first_name: {
+        value: first_name,
+        type: "alpha"
+      },
+      last_name: {
+        value: last_name,
+        type: "alpha"
+      },
+      role_id: {
+        value: role_id,
+        type: "int"
+      },
+      date_of_birth: {
+        value: date_of_birth,
+        type: "date"
+      },
+      email: {
+        value: email,
+        type: "email"
+      }
+    });
+
+    if (gender !== undefined) {
+      validateFields({
+        gender: {
+          value: gender,
+          type: "gender"
+        }
+      });
+    }
+
+    gender = gender || null;
+
+    await UsersService.createUser(
+      role_id,
+      email,
+      first_name,
+      last_name,
+      gender,
+      date_of_birth
+    );
+
+    res.status(201).end();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
--- backend/Users/services.js
+++ backend/Users/services.js
@@ -4,6 +4,21 @@ const getAll = async () => {
   return await query("SELECT * FROM users");
 };
 
+const createUser = async (
+  role_id,
+  email,
+  first_name,
+  last_name,
+  gender,
+  date_of_birth
+) => {
+  return await query(
+    "INSERT INTO users (role_id, email, first_name, last_name, gender, date_of_birth) VALUES ($1, $2, $3, $4, $5, $6)",
+    [role_id, email, first_name, last_name, gender, date_of_birth]
+  );
+};
+
 module.exports = {
-  getAll
+  getAll,
+  createUser
 };
