7cb6110fb82e4e52c088366ea335af47a71ceec2
Gabriel-David <rosudavidg@gmail.com>
2020-03-20 00:14:43 +0200

    added validator

---
+++ backend/utils/index.js
@@ -0,0 +1,71 @@
+const validator = require("validator");
+
+const { ServerError } = require("../errors");
+
+const validateFields = fields => {
+  for (let fieldName in fields) {
+    const fieldValue = fields[fieldName].value + "";
+    const fieldType = fields[fieldName].type;
+
+    if (!fieldValue) {
+      throw new ServerError(`Lipseste campul ${fieldName}`, 400);
+    }
+
+    switch (fieldType) {
+      case "ascii":
+        if (!validator.isAscii(fieldValue)) {
+          throw new ServerError(
+            `Campul ${fieldName} trebuie sa contina doar caractere ascii`,
+            400
+          );
+        }
+        break;
+      case "alpha":
+        if (!validator.isAlpha(fieldValue)) {
+          throw new ServerError(
+            `Campul ${fieldName} trebuie sa contina doar litere`,
+            400
+          );
+        }
+        break;
+      case "int":
+        if (!validator.isInt(fieldValue)) {
+          throw new ServerError(
+            `Campul ${fieldName} trebuie sa fie un numar intreg`,
+            400
+          );
+        }
+        break;
+      case "jwt":
+        if (!validator.isJWT(fieldValue)) {
+          throw new ServerError(`Campul ${fieldName} trebuie sa fie jwt`, 400);
+        }
+        break;
+      case "date":
+        if (!validator.toDate(fieldValue)) {
+          throw new ServerError(`Campul ${fieldName} trebuie sa fie date`, 400);
+        }
+        break;
+      case "email":
+        if (!validator.isEmail(fieldValue)) {
+          throw new ServerError(
+            `Campul ${fieldName} trebuie sa fie email`,
+            400
+          );
+        }
+        break;
+      case "gender":
+        if (fieldValue !== "M" && fieldValue !== "F") {
+          throw new ServerError(
+            `Campul ${fieldName} trebuie sa fie gender`,
+            400
+          );
+        }
+        break;
+    }
+  }
+};
+
+module.exports = {
+  validateFields
+};
