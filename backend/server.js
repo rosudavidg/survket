82a04af334637603f00e87645e63a26875272fb2
Gabriel-David <rosudavidg@gmail.com>
2020-03-20 00:15:01 +0200

    added errors middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,22 @@ const app = express();
 const PORT = 5000;
 const HOST = "0.0.0.0";
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/", routes);
+
+app.use("/", (err, req, res, next) => {
+  console.trace(err);
+  let status = 500;
+  let message = "Something Bad Happened";
+  if (err.httpStatus) {
+    status = err.httpStatus;
+    message = err.message;
+  }
+  res.status(status).json({
+    error: message
+  });
+});
 
 app.listen(PORT, HOST);
