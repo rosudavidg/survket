860e4414c94ea6a2ca1a60ed73f8a32d4e3e5fc9
Gabriel-David <rosudavidg@gmail.com>
2020-03-26 16:46:57 +0200

    fixed POST /survey and GET /surveys

--- backend/Surveys/services.js
+++ backend/Surveys/services.js
@@ -1,23 +1,76 @@
 const query = require("../database");
 
 const create = async (userId, name, reward, surveys_texts, surveys_choices) => {
+  // Insert into surveys
   let rows = await query(
     "INSERT INTO surveys (creator, name, reward) VALUES ($1, $2, $3) RETURNING *",
     [userId, name, reward]
   );
 
   let surveyId = rows[0].id;
+
+  // Insert into surveys_texts
+  for (let i = 0; i < surveys_texts.length; i++) {
+    await query(
+      "INSERT INTO surveys_texts (survey_id, question) VALUES ($1, $2)",
+      [surveyId, surveys_texts[i].question]
+    );
+  }
+
+  // Insert into surveys_choices
+  for (let i = 0; i < surveys_choices.length; i++) {
+    let rows = await query(
+      "INSERT INTO surveys_choices (survey_id, question) VALUES ($1, $2) RETURNING *",
+      [surveyId, surveys_choices[i].question]
+    );
+    let survey_choice_id = rows[0].id;
+
+    let survey_choice_elements = surveys_choices[i].surveys_choices_elements;
+
+    // Insert into surveys_choices_elements
+    for (let j = 0; j < survey_choice_elements.length; j++) {
+      await query(
+        "INSERT INTO surveys_choices_elements (survey_choice_id, text) VALUES ($1, $2)",
+        [survey_choice_id, surveys_choices[i].surveys_choices_elements[j].text]
+      );
+    }
+  }
 };
 
 const getAll = async () => {
+  // Get surveys
   let surveys = await query("SELECT * FROM surveys");
 
   for (let i = 0; i < surveys.length; i++) {
     let survey = surveys[i];
+
+    // Get surveys_texts
     let surveys_text = await query(
-      "SELECT * FROM surveys_texts WHERE id = $1",
+      "SELECT id, question FROM surveys_texts WHERE survey_id = $1",
       [survey.id]
     );
+
+    survey["surveys_texts"] = surveys_text;
+
+    // Get surveys_choices
+    let surveys_choices = await query(
+      "SELECT * FROM surveys_choices WHERE survey_id = $1",
+      [survey.id]
+    );
+
+    survey["surveys_choices"] = [];
+
+    // Get surveys_choices_elements
+    for (let j = 0; j < surveys_choices.length; j++) {
+      let surveys_choices_elements = await query(
+        "SELECT id, text FROM surveys_choices_elements WHERE survey_choice_id = $1",
+        [surveys_choices[j].id]
+      );
+      survey["surveys_choices"].push({
+        question: surveys_choices[j].question,
+        surveys_choices_elements
+      });
+    }
   }
 
   return surveys;
