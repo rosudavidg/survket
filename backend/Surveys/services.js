f92bd755cf6ae02a55cd6327faa8ecfdf3922338
Gabriel-David <rosudavidg@gmail.com>
2020-04-26 01:38:16 +0300

    fixed creator field

--- backend/Surveys/services.js
+++ backend/Surveys/services.js
@@ -3,63 +3,62 @@ const { ServerError } = require("../errors");
 
 const create = async (userId, name, reward, surveys_texts, surveys_choices) => {
   // Insert into surveys
-  let rows = await query(
-    "INSERT INTO surveys (creator, name, reward) VALUES ($1, $2, $3) RETURNING *",
-    [userId, name, reward]
-  );
+  let rows = await query("INSERT INTO surveys (creator, name, reward) VALUES ($1, $2, $3) RETURNING *", [
+    userId,
+    name,
+    reward,
+  ]);
 
   let surveyId = rows[0].id;
 
   // Insert into surveys_texts
   for (let i = 0; i < surveys_texts.length; i++) {
-    await query(
-      "INSERT INTO surveys_texts (survey_id, question) VALUES ($1, $2)",
-      [surveyId, surveys_texts[i].question]
-    );
+    await query("INSERT INTO surveys_texts (survey_id, question) VALUES ($1, $2)", [
+      surveyId,
+      surveys_texts[i].question,
+    ]);
   }
 
   // Insert into surveys_choices
   for (let i = 0; i < surveys_choices.length; i++) {
-    let rows = await query(
-      "INSERT INTO surveys_choices (survey_id, question) VALUES ($1, $2) RETURNING *",
-      [surveyId, surveys_choices[i].question]
-    );
+    let rows = await query("INSERT INTO surveys_choices (survey_id, question) VALUES ($1, $2) RETURNING *", [
+      surveyId,
+      surveys_choices[i].question,
+    ]);
     let survey_choice_id = rows[0].id;
 
     let survey_choice_elements = surveys_choices[i].surveys_choices_elements;
 
     // Insert into surveys_choices_elements
     for (let j = 0; j < survey_choice_elements.length; j++) {
-      await query(
-        "INSERT INTO surveys_choices_elements (survey_choice_id, text) VALUES ($1, $2)",
-        [survey_choice_id, surveys_choices[i].surveys_choices_elements[j].text]
-      );
+      await query("INSERT INTO surveys_choices_elements (survey_choice_id, text) VALUES ($1, $2)", [
+        survey_choice_id,
+        surveys_choices[i].surveys_choices_elements[j].text,
+      ]);
     }
   }
 };
 
 const getAll = async (userId) => {
   // Get surveys
-  let surveys = await query("SELECT * FROM surveys WHERE creator = $1", [
-    userId,
-  ]);
+  let surveys = await query("SELECT * FROM surveys WHERE creator = $1", [userId]);
 
   for (let i = 0; i < surveys.length; i++) {
     let survey = surveys[i];
 
     // Get surveys_texts
-    let surveys_text = await query(
-      "SELECT id, question FROM surveys_texts WHERE survey_id = $1",
-      [survey.id]
-    );
+    let surveys_text = await query("SELECT id, question FROM surveys_texts WHERE survey_id = $1", [survey.id]);
 
+    // Replace creator id with company name
+    const company_name = (await query("SELECT company_name FROM creator_users WHERE id = $1", [survey["creator"]]))[0][
+      "company_name"
+    ];
+
+    survey["creator"] = company_name;
     survey["surveys_texts"] = surveys_text;
 
     // Get surveys_choices
-    let surveys_choices = await query(
-      "SELECT * FROM surveys_choices WHERE survey_id = $1",
-      [survey.id]
-    );
+    let surveys_choices = await query("SELECT * FROM surveys_choices WHERE survey_id = $1", [survey.id]);
 
     survey["surveys_choices"] = [];
 
@@ -87,18 +86,19 @@ const getAllAsSolver = async () => {
     let survey = surveys[i];
 
     // Get surveys_texts
-    let surveys_text = await query(
-      "SELECT id, question FROM surveys_texts WHERE survey_id = $1",
-      [survey.id]
-    );
+    let surveys_text = await query("SELECT id, question FROM surveys_texts WHERE survey_id = $1", [survey.id]);
+
+    // Replace creator id with company name
+    const company_name = (await query("SELECT company_name FROM creator_users WHERE id = $1", [survey["creator"]]))[0][
+      "company_name"
+    ];
+
+    survey["creator"] = company_name;
 
     survey["surveys_texts"] = surveys_text;
 
     // Get surveys_choices
-    let surveys_choices = await query(
-      "SELECT * FROM surveys_choices WHERE survey_id = $1",
-      [survey.id]
-    );
+    let surveys_choices = await query("SELECT * FROM surveys_choices WHERE survey_id = $1", [survey.id]);
 
     survey["surveys_choices"] = [];
 
@@ -120,30 +120,20 @@ const getAllAsSolver = async () => {
 
 const getById = async (userId, surveyId) => {
   // Get surveys
-  let surveys = await query(
-    "SELECT * FROM surveys WHERE creator = $1 and id = $2",
-    [userId, surveyId]
-  );
+  let surveys = await query("SELECT * FROM surveys WHERE creator = $1 and id = $2", [userId, surveyId]);
 
-  if (surveys.length != 1)
-    throw new ServerError(`Survey ${surveyId} not found.`, 400);
+  if (surveys.length != 1) throw new ServerError(`Survey ${surveyId} not found.`, 400);
 
   for (let i = 0; i < surveys.length; i++) {
     let survey = surveys[i];
 
     // Get surveys_texts
-    let surveys_text = await query(
-      "SELECT id, question FROM surveys_texts WHERE survey_id = $1",
-      [survey.id]
-    );
+    let surveys_text = await query("SELECT id, question FROM surveys_texts WHERE survey_id = $1", [survey.id]);
 
     survey["surveys_texts"] = surveys_text;
 
     // Get surveys_choices
-    let surveys_choices = await query(
-      "SELECT * FROM surveys_choices WHERE survey_id = $1",
-      [survey.id]
-    );
+    let surveys_choices = await query("SELECT * FROM surveys_choices WHERE survey_id = $1", [survey.id]);
 
     survey["surveys_choices"] = [];
 
@@ -168,25 +158,18 @@ const getByIdAsSolver = async (surveyId) => {
   // Get surveys
   let surveys = await query("SELECT * FROM surveys WHERE id = $1", [surveyId]);
 
-  if (surveys.length != 1)
-    throw new ServerError(`Survey ${surveyId} not found.`, 400);
+  if (surveys.length != 1) throw new ServerError(`Survey ${surveyId} not found.`, 400);
 
   for (let i = 0; i < surveys.length; i++) {
     let survey = surveys[i];
 
     // Get surveys_texts
-    let surveys_text = await query(
-      "SELECT id, question FROM surveys_texts WHERE survey_id = $1",
-      [survey.id]
-    );
+    let surveys_text = await query("SELECT id, question FROM surveys_texts WHERE survey_id = $1", [survey.id]);
 
     survey["surveys_texts"] = surveys_text;
 
     // Get surveys_choices
-    let surveys_choices = await query(
-      "SELECT * FROM surveys_choices WHERE survey_id = $1",
-      [survey.id]
-    );
+    let surveys_choices = await query("SELECT * FROM surveys_choices WHERE survey_id = $1", [survey.id]);
 
     survey["surveys_choices"] = [];
 
@@ -212,28 +195,25 @@ let solve = async (surveyId, userId, surveys_text, surveys_choices) => {
   try {
     await query("BEGIN");
 
-    rows = await query(
-      "INSERT INTO solved_surveys (survey_id, user_id) VALUES ($1, $2) RETURNING *",
-      [surveyId, userId]
-    );
+    rows = await query("INSERT INTO solved_surveys (survey_id, user_id) VALUES ($1, $2) RETURNING *", [
+      surveyId,
+      userId,
+    ]);
 
     solved_survey_id = rows[0].id;
 
     for (let i = 0; i < surveys_text.length; i++) {
-      await query(
-        "INSERT INTO solved_surveys_texts (solved_survey_id, survey_text_id, answer) VALUES ($1, $2, $3)",
-        [solved_survey_id, surveys_text[i].id, surveys_text[i].answer]
-      );
+      await query("INSERT INTO solved_surveys_texts (solved_survey_id, survey_text_id, answer) VALUES ($1, $2, $3)", [
+        solved_survey_id,
+        surveys_text[i].id,
+        surveys_text[i].answer,
+      ]);
     }
 
     for (let i = 0; i < surveys_choices.length; i++) {
       await query(
         "INSERT INTO solved_surveys_choices (solved_survey_id, survey_choice_id, survey_choice_element_id) VALUES ($1, $2, $3)",
-        [
-          solved_survey_id,
-          surveys_choices[i].id,
-          surveys_choices[i].survey_choice_element_id,
-        ]
+        [solved_survey_id, surveys_choices[i].id, surveys_choices[i].survey_choice_element_id]
       );
     }
     await query("COMMIT");
@@ -244,27 +224,15 @@ let solve = async (surveyId, userId, surveys_text, surveys_choices) => {
 };
 
 let getStats = async (userId, surveyId) => {
-  let rows = await query(
-    "SELECT * FROM surveys WHERE id = $1 AND creator = $2",
-    [surveyId, userId]
-  );
+  let rows = await query("SELECT * FROM surveys WHERE id = $1 AND creator = $2", [surveyId, userId]);
 
   let survey = rows[0];
 
-  let surveys_texts = await query(
-    "SELECT id, question FROM surveys_texts WHERE survey_id = $1",
-    [surveyId]
-  );
+  let surveys_texts = await query("SELECT id, question FROM surveys_texts WHERE survey_id = $1", [surveyId]);
 
-  let surveys_choices = await query(
-    "SELECT id, question FROM surveys_choices WHERE survey_id = $1",
-    [surveyId]
-  );
+  let surveys_choices = await query("SELECT id, question FROM surveys_choices WHERE survey_id = $1", [surveyId]);
 
-  let solved_surveys_ids = await query(
-    "SELECT id FROM solved_surveys WHERE survey_id = $1",
-    [surveyId]
-  );
+  let solved_surveys_ids = await query("SELECT id FROM solved_surveys WHERE survey_id = $1", [surveyId]);
 
   let surveys_texts_stats = [];
 
