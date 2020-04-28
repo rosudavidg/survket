7ea964d5913387c18f1e7792c9257eba73f6a3dc
Gabriel-David <rosudavidg@gmail.com>
2020-04-28 22:43:23 +0300

    added Nav buttons

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { isUserAuthenticated } from "./Auth.js";
 import { useHistory } from "react-router-dom";
+import "./Nav.css";
 
 const Nav = () => {
   const history = useHistory();
@@ -10,10 +11,35 @@ const Nav = () => {
     history.push("/");
   };
 
+  const onClickHome = () => {
+    history.push("/");
+  };
+
+  const onClickFAQ = () => {
+    history.push("/faq");
+  };
+
+  const onClickContact = () => {
+    history.push("/contact");
+  };
+
   if (isUserAuthenticated(localStorage.getItem("token"))) {
-    return <input type="submit" value="Sign out" onClick={onClickSignOut}></input>;
+    return (
+      <>
+        <input className="nav-home" type="submit" value="Home" onClick={onClickHome}></input>
+        <input className="nav-faq" type="submit" value="FAQ" onClick={onClickFAQ}></input>
+        <input className="nav-contact" type="submit" value="Contact" onClick={onClickContact}></input>
+        <input className="nav-signout" type="submit" value="Sign out" onClick={onClickSignOut}></input>
+      </>
+    );
   } else {
-    return <></>;
+    return (
+      <>
+        <input className="nav-home" type="submit" value="Home" onClick={onClickHome}></input>
+        <input className="nav-faq" type="submit" value="FAQ" onClick={onClickContact}></input>
+        <input className="nav-contact" type="submit" value="Contact" onClick={onClickSignOut}></input>
+      </>
+    );
   }
 };
 
