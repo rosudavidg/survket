3987e9dd227520e3643054cc7c59aceae64c53fd
Gabriel-David <rosudavidg@gmail.com>
2020-04-29 00:14:00 +0300

    added route for Questions

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,10 @@ const Router = require("express")();
 
 const UsersController = require("../Users/controller.js");
 const SurveysController = require("../Surveys/controller.js");
+const QuestionsController = require("../Questions/controller.js");
 
 Router.use("/users", UsersController);
 Router.use("/surveys", SurveysController);
+Router.use("/questions", QuestionsController);
 
 module.exports = Router;
