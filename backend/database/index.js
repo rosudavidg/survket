4b5c2b57e8d99349b02aae3698cbce346996465f
Gabriel-David <rosudavidg@gmail.com>
2020-05-02 15:30:22 +0300

    removed timeout

--- backend/database/index.js
+++ backend/database/index.js
@@ -7,10 +7,6 @@ const pool = new Pool({
   database: fs.readFileSync(process.env.DATABASE_NAME, "utf8"),
   password: fs.readFileSync(process.env.DATABASE_PASSWORD, "utf8"),
   port: process.env.DATABASE_PORT,
-  // ssl: true,
-  idleTimeoutMillis: 1000, // close idle clients after 1 second
-  connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
-  maxUses: 7500,
 });
 
 const query = async (text, params) => {
